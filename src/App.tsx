14a5fb44320138d96c84d964382495fd37115032
Buleon Benjamin <buleon.benjamin@gmail.com>
2024-01-10 09:01:05 +0000

    turn into short version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import LoginForm from "./components/LoginForm";
 import Title from "./components/Title";
-import { Content } from "./components/content";
-import "./components/styles.css"
+import "./components/styles.css";
 
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
         <div className="m-4">
           <Title>Welcome to Gojob !</Title>
         </div>
-        <Content />
+        <LoginForm />
       </div>
     </div>
   );
